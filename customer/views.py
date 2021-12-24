9eea9ff6db5dc043140ead81678ce670c1bfee3c
vilsonlopes <vilsonlopes@yahoo.com.br>
2021-12-24 19:58:29 -0300

    adição exclusão e busca

--- customer/views.py
+++ customer/views.py
@@ -1,18 +1,30 @@
 from django.forms import forms
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, UpdateView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from .models import Customer
 from .forms import CustomerForm
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 # Create your views here.
 
 
 class CustomerListView(ListView):
     template_name = "customer/customer_list.html"
+    paginate_by = 2
     model = Customer
-    queryset = Customer.objects.all()
+#    queryset = Customer.objects.all() # Desativado e criado uma função para fazer buscas
+
+    def get_queryset(self):
+        name = self.request.GET.get("name")
+        if name:
+            object_list = self.model.objects.filter(
+                Q(first_name__icontains=name) | Q(last_name__icontains=name)
+            )
+        else:
+            object_list = self.model.objects.all()
+        return object_list
 
 
 class CustomerCreateView(CreateView):
@@ -36,6 +48,16 @@ class CustomerUpdateView(UpdateView):
 
     def form_valid(self, form):
         return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse("customer:customer-list")
+
+
+class CustomerDeleteView(DeleteView):
+
+    def get_object(self, queryset=None):
+        id = self.kwargs.get("id")
+        return get_object_or_404(Customer, id=id)
 
     def get_success_url(self):
         return reverse("customer:customer-list")
